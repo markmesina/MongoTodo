d3bea09c3c352b1df56f7c4a6d625664d758a59a
ceress <markmesina69@gmail.com>
2020-05-06 20:08:39 -0700

    added controller commands

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,18 @@
+const { User } = require('./../models');
+const { isEmail, isLength } = require('validator');
+
 module.exports = {
     signup: async (req,res) => {
-        res.send('Hello World');
+     const { email, password } = req.body;
+     
+     if(!email || !password) {
+         return res.status(400).json({ error: 'You must provide an email and password' });
+     }
+     if(!isEmail(email)) {
+         return res.status(403).json({ error: 'You must provide a valid email address' });
+     }
+     if(!isLength(password, { min: 6 })) {
+         return res.status(403).json({ error: 'Your password must at least be 6 characters long'});
+     }
     },
 };
--- routes/apiRoutes/authRoutes/index.js
+++ routes/apiRoutes/authRoutes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const { signup } = require('./../../../controllers/authController');
 // /api/auth/signup
-router.post('signup', signup);
+router.post('/signup', signup);
 
 
 module.exports = router;
